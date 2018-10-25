9f44da434679a399d30ae64d1550fa6ed7628ce7
Joe Timmins <joe@novoda.com>
2018-10-25 15:40:28 +0100

    Kotlin-ise the Item class

--- kata/src/main/java/com/novoda/kata/Item.kt
+++ kata/src/main/java/com/novoda/kata/Item.kt
@@ -1,11 +1,3 @@
 package com.novoda.kata
 
-class Item(/* Generated getter and setter code */var name: String, sellIn: Int, quality: Int) {
-    var sellIn: Int = 0
-    var quality: Int = 0
-
-    init {
-        this.sellIn = sellIn
-        this.quality = quality
-    }
-}
+class Item(var name: String, var sellIn: Int = 0, var quality: Int = 0)
