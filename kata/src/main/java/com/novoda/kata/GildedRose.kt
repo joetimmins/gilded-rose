caf31af27ddbf75839c4c01c1c9bb8ceaed62130
Joe Timmins <joe@novoda.com>
2018-10-25 19:45:01 +0100

    Invert boolean condition for brie

--- kata/src/main/java/com/novoda/kata/GildedRose.kt
+++ kata/src/main/java/com/novoda/kata/GildedRose.kt
@@ -31,21 +31,19 @@ object GildedRose {
     fun updateInventory(items: MutableList<Item>): MutableList<Item> {
         items.forEach { item ->
             val isNotLegendary = "Sulfuras, Hand of Ragnaros" != item.name
-            val isNotBrie = "Aged Brie" != item.name
+            val isBrie = "Aged Brie" == item.name
             val isBackstagePass = "Backstage passes to a TAFKAL80ETC concert" == item.name
             val isConjured = item.name.startsWith("Conjured")
 
-            if (isNotBrie && !isBackstagePass) {
+            if (!isBrie && !isBackstagePass) {
                 handleConjuredAndLegendaryCases(isConjured, item, isNotLegendary)
             } else {
                 item.updateQuality(1)
 
                 if (isBackstagePass) {
-                    if (item.sellIn < 11) {
-                        item.updateQuality(1)
-                    }
-
                     if (item.sellIn < 6) {
+                        item.updateQuality(2)
+                    } else if (item.sellIn < 11) {
                         item.updateQuality(1)
                     }
                 }
@@ -56,15 +54,12 @@ object GildedRose {
             }
 
             if (item.sellIn < 0) {
-                if (isNotBrie) {
-                    if (!isBackstagePass) {
-                        handleConjuredAndLegendaryCases(isConjured, item, isNotLegendary)
-                    } else {
-                        item.quality = 0
-                    }
-                } else {
-                    item.updateQuality(1)
+                when {
+                    isBrie -> item.updateQuality(1)
+                    isBackstagePass -> item.quality = 0
+                    else -> handleConjuredAndLegendaryCases(isConjured, item, isNotLegendary)
                 }
+
             }
         }
         return items
